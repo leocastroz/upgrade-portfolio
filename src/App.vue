751626d549095e16e5abb3f29903d442428fbb26
leocastroz <castroz.dev@gmail.com>
2022-11-06 22:28:46 -0300

    Diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <TheHeader />
-  <img 
+  <TheHeader 
+    v-if="showHeader"
+  />
+
+  <br>
+
+  <div v-show="showName">
+    Nome: {{ firstName }}
+    <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <br>
+
+  <div v-if="accessLevel === 'admin'">ADMIN</div>
+  <div v-else-if="accessLevel === 'user'">USER</div>
+  <div v-else>ANONIMO</div>
+
+  <img
   alt="Vue logo" 
   src="./assets/logo.png"
   >
@@ -16,6 +33,15 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  data () {
+    return{
+      showHeader: true,
+      firstName: 'Leonardo',
+      lastName: 'Silva Castro',
+      showName: true,
+      accessLevel: 'use'
+    }
   }
 }
 </script>
